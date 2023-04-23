83747f111e2f4851d0babad30c060ce293022631
williamdeniro3 <WilliamDeNiro3@gmail.com>
2023-04-23 13:06:53 -0400

    bad spelling

--- src/utils/ChatGPT.js
+++ src/utils/ChatGPT.js
@@ -14,7 +14,7 @@ class ChatGPT {
 
         const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{role: "system", content: "Can help me tailor me resume to the specific job description. I want you to provide me with tailored bullet points that highlight my most relevant skills and experiences for the position."
+        messages: [{role: "system", content: "Can you help me tailor me resume to the specific job description. I want you to provide me with tailored bullet points that highlight my most relevant skills and experiences for the position."
             + "Here is the resume \n" + resume + "\n Here is the Job Description \n" + jobDescription
     
     }],
