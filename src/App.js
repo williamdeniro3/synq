b7edf25968d55aee32ba74b8b92e559360da2e3f
williamdeniro3 <WilliamDeNiro3@gmail.com>
2023-04-23 17:01:13 -0400

    Added and organized code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
+import AnalysisComponent from './components/AnalysisComponent';
 import './styles.css';
 
 function App() {
-  const handleClick = () => {
-      // do something with Connor's branch
-  };
+
 
   return (
       <div className="container">
-        <button type="button" className="btn btn-dark btn-lg"  onClick={handleClick}>GPT Call</button>
+          <AnalysisComponent/>
       </div>
   );
 }
---
+++ src/components/AnalysisComponent.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import ChatGPT from '../utils/ChatGPT';
+
+function AnalysisComponent() {
+  const [response, setResponse] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const chatbot = new ChatGPT();
+    const response = await chatbot.getTailoredBulletPoints(process.env.REACT_APP_DUMMY_RESUME, process.env.REACT_APP_DUMMY_JOB_APPLICATION);
+    setResponse(response);
+  }
+
+  return (
+        //Connor put the text fields inbetween the opening Form tag and the
+    <div>
+      <form onSubmit={handleSubmit}>
+
+        <button className='btn btn-primary'>Get Response</button>
+      </form>
+      {response && <p>{response}</p>}
+    </div>
+  );
+}
+
+export default AnalysisComponent;
